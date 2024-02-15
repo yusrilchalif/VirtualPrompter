c8eca9d1243f890d5a48fa21c1505eef1e72155d
yusril <yusrilchalif@gmail.com>
2024-02-15 10:15:30 +0700

    fix timer handler

--- Assets/Script/TimerHandler.cs
+++ Assets/Script/TimerHandler.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TimerHandler : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float timeRemaining = 300f;
+    [SerializeField] Color timeupColor = Color.red;
+
+    public bool isRun = false;
+     
     // Start is called before the first frame update
     void Start()
     {
-        
+        isRun = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if(isRun)
+        //{
+        //    if(timeRemaining > 0)
+        //    {
+        //        timeRemaining -= Time.deltaTime;
+        //        DisplayTimer(timeRemaining);
+        //    }
+        //    else
+        //    {
+        //        print("time has run out");
+        //        //timeRemaining = 0;
+        //        DisplayTimer(0);
+
+        //        //isRun = false;
+
+        //        ChangeColorText(timeupColor);
+        //    }
+        //}
+
+        if(isRun)
+        {
+            timeRemaining -= Time.deltaTime;
+
+            if(timeRemaining <= 0)
+            {
+                print("time out");
+                ChangeColorText(timeupColor);
+            }
+
+            DisplayTimer(timeRemaining);
+        }
+    }
+
+    void DisplayTimer(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        string sign = (timeToDisplay < 0) ? "-" : "";
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void ChangeColorText(Color color)
+    {
+        timerText.color = color;
     }
 }
