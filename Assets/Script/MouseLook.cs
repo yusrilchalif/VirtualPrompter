1cd8fc76fcac048a3ac38c32c7345b3b924103e4
yusril <yusrilchalif@gmail.com>
2024-02-21 09:15:42 +0700

    fix fps player movement

---
+++ Assets/Script/MouseLook.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitify = 100f;
+    public Transform playerBody;
+
+    private float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the left mouse button is held down
+        if (Input.GetMouseButton(0))
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitify * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitify * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
